c6ec6050ea11cdd7c0624c9db9796d100160269e
Pranav <mpranavprem@gmail.com>
2025-02-19 18:39:48 +0530

    changes

--- src/components/website/Header.jsx
+++ src/components/website/Header.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Divide as Hamburger } from "hamburger-react";
-import { logo } from "../../content/constant";
+import { companyDetails, logo } from "../../content/constant";
 import Drawer from "react-modern-drawer";
 import { Link, useLocation } from "react-router-dom";
 import { IoClose } from "react-icons/io5";
@@ -52,7 +52,10 @@ const Header = () => {
             ))}
           </div>
           <div data-aos="fade-down" className="hidden lg:flex">
-            <Link href="/" className="group relative w-[70px] h-[70px]">
+            <Link
+              to={`tel:${companyDetails.phone}`}
+              className="group relative w-[70px] h-[70px]"
+            >
               <div className="group-hover:bg-[#b81e23] z-[1] flex items-center justify-center absolute top-1/2 left-0 -translate-y-1/2 h-[31px] w-full bg-[#E41B23] rounded-[6px] text-sm transition duration-300">
                 Call Us
               </div>
